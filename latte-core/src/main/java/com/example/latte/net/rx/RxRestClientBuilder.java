8a66db55f0daaba3b9c65e70ede31aad93457b93
tangchen <tangchen@getein.cn>
2017-10-09 16:25:08 +0800

    Retrofit与RxJava的整合，实现网络请求的响应式编程

---
+++ latte-core/src/main/java/com/example/latte/net/rx/RxRestClientBuilder.java
@@ -0,0 +1,82 @@
+package com.example.latte.net.rx;
+
+import android.content.Context;
+
+import com.example.latte.net.RestClient;
+import com.example.latte.net.RestCreator;
+import com.example.latte.net.callback.IError;
+import com.example.latte.net.callback.IFailure;
+import com.example.latte.net.callback.IRequest;
+import com.example.latte.net.callback.ISuccess;
+import com.example.latte.ui.LoaderStyle;
+
+import java.io.File;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+
+/**
+ * Created by GPT-2273 on 2017/9/25.
+ */
+
+public class RxRestClientBuilder {
+
+    private String mUrl = null;
+    private static final Map<String, Object> PARAMS = RestCreator.getParams();
+    private RequestBody mBody = null;
+    private File mFile = null;
+    private LoaderStyle mLoaderStyle = null;
+    private Context mContext = null;
+
+    RxRestClientBuilder() {
+
+    }
+
+    public final RxRestClientBuilder url(String url) {
+        this.mUrl = url;
+        return this;
+    }
+
+    public final RxRestClientBuilder params(Map<String, Object> params) {
+        PARAMS.putAll(params);
+        return this;
+    }
+
+    public final RxRestClientBuilder params(String key, Object value) {
+        PARAMS.put(key, value);
+        return this;
+    }
+
+    public final RxRestClientBuilder file(File file) {
+        this.mFile = file;
+        return this;
+    }
+
+    public final RxRestClientBuilder file(String file) {
+        this.mFile = new File(file);
+        return this;
+    }
+
+    public final RxRestClientBuilder raw(String raw) {
+        this.mBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), raw);
+        return this;
+    }
+
+    public final RxRestClientBuilder loader(LoaderStyle loaderStyle, Context context) {
+        this.mLoaderStyle = loaderStyle;
+        this.mContext = context;
+        return this;
+    }
+
+    public final RxRestClientBuilder loader(Context context) {
+        this.mContext = context;
+        this.mLoaderStyle = LoaderStyle.BallClipRotatePulseIndicator;
+        return this;
+    }
+
+    public final RxRestClient build() {
+        return new RxRestClient(mUrl, PARAMS, mBody, mFile, mLoaderStyle, mContext);
+    }
+
+}
